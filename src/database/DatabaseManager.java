8c11eec6a54a97806921783e0a9428577b48d595
KristianColville1 <kristiancolville1@gmail.com>
2024-01-01 12:09:47 +0000

    Scrap DAO and focus on generic ORMs for controllers

---
+++ src/database/DatabaseManager.java
@@ -0,0 +1,22 @@
+package database;
+
+import database.DatabaseConnector;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    public static <T> boolean insert(T entity) {
+        return false;
+    }
+
+    public static <T> boolean update(T entity) {
+        return false;
+    }
+
+    public static <T> boolean delete(T entity) {
+        return false;
+    }
+}
